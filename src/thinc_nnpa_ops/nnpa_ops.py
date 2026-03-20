7696b30bb9e771dfd3029bffc54ad8028a2dca12
Tabari Alexander <tehbone@users.noreply.github.com>
2026-03-20 16:05:05 -0400

    Add support from out of tree definitions

--- src/thinc_nnpa_ops/nnpa_ops.py
+++ src/thinc_nnpa_ops/nnpa_ops.py
@@ -1,9 +1,16 @@
 from thinc_bigendian_ops import BigEndianOps
 from thinc.config import registry
+from thinc.compat import torch
 
-@registry.ops("NnpaOps")
+@registry.ops("nnpa")
 class NnpaOps(BigEndianOps):
     """Thinc Ops class that handles usage of Telum acceleration.
     Basic operations fall back to the big-endian numpy implementation.
     This largely enables the use of the nnpa device for pytorch models."""
     name = "nnpa"
+
+    def has_gpu_support(self):
+        return torch != None and hasattr(torch, "nnpa")
+
+    def get_default_torch_device(self):
+        return torch.device("nnpa")
